bcfc2e225a7fcb3fcfa368902d5c34b947024661
Sergio_Andres_Lopez_Sepulveda <ser.and60@gmail.com>
2025-12-03 19:11:36 -0500

    Creación del schema.prisma y CORS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("<h1>Maracuya Run</h1>");
+  res.send("<button>Maracuya Run</button>");
 });
 
 app.listen(PORT, () => {
